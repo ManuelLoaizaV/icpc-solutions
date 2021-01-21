1b890f7e21d4c49d2080f4901b1794fe1923ae47
ManuelLoaizaVasquez <manulova999@gmail.com>
2021-01-21 18:47:18 -0500

    dp bitmask

--- dynamic_programming/Assignments.cpp
+++ dynamic_programming/Assignments.cpp
@@ -1,83 +1,61 @@
-// Gracias, Rodolfo
-// Gracias, MarcosK
-// Gracias, Graphter
-// Obrigado, Dilson
-//#pragma GCC optimize ("Ofast,unroll-loops")
-//#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
-#include <bits/stdc++.h>
-#include <unistd.h>
+//https://www.spoj.com/problems/ASSIGN/
+#include <iostream>
 using namespace std;
-#define fastio ios::sync_with_stdio(0);cin.tie(0)
-#define For(i,a,b) for (Long i = a; i < b; i++)
-#define roF(i,a,b) for (Long i = a; i >= b; i--)
-#define pb push_back
-#define mp make_pair
-#define ff first
-#define ss second
-#define all(v) (v).begin(),(v).end()
 
 typedef long long Long;
-typedef long double Double;
-typedef unsigned long long ULong;
-typedef pair<Long, Long> Pair;
-typedef vector<Long> Vector;
-typedef vector<Pair> PairVector;
 
 const int N = 20;
 const int MASK = 1 << 20;
-const Long INF = 1e18;
-const Double EPS = 10e-9;
 
-Long n;
+int n;
 bool likes[N][N];
-Long dp[N][MASK];
-bool done[N][MASK];
 
-Long get_bit(Long mask, Long bit) {
-	return (mask >> bit) & 1LL;
+bool is_done[MASK][N];
+Long dp[MASK][N];
+
+Long GetBit(Long mask, Long bit) {
+  return (mask >> bit) & 1LL;
 }
 
-void turn_on(Long& mask, Long bit) {
-	mask = mask | (1LL << bit);
+void TurnOn(Long& mask, Long bit) {
+  mask = mask | (1LL << bit);
 }
 
-void turn_off(Long& mask, Long bit) {
-	mask = mask & (~(1 << bit));
+void TurnOff(Long& mask, Long bit) {
+  mask = mask & (~(1 << bit));
 }
 
-void clear(Long n) {
-	Long lim = 1 << n;
-	For(i, 0, n) For(j, 0, lim) done[i][j] = false;
+Long DP(Long mask, Long who) {
+  if (who == n) return 1;
+  if (is_done[mask][who]) return dp[mask][who];
+  dp[mask][who] = 0;
+  Long aux = mask;
+  for (int bit = 0; bit < n; bit++) {
+    if (GetBit(mask, bit) == 0 && likes[who][bit]) {
+      TurnOn(aux, bit);
+      dp[mask][who] += DP(aux, who + 1);
+      TurnOff(aux, bit);
+    }
+  }  
+  is_done[mask][who] = true;
+  return dp[mask][who];
 }
 
-// DP(pos, mask) : cantidad de maneras de distribuir las asignaturas
-// entre los alumnos [pos ... n - 1] habiendo utilizado los cursos con 1 en mask
-Long DP(Long pos, Long mask) {
-	if (pos == n) return 1LL;
-	if (done[pos][mask]) return dp[pos][mask];
-	dp[pos][mask] = 0;
-	For(bit, 0, n) {
-		if (get_bit(mask, bit) == 0 and likes[pos][bit]) {
-			Long new_mask = mask;
-			turn_on(new_mask, bit);
-			dp[pos][mask] += DP(pos + 1, new_mask);
-		}
-	}
-	done[pos][mask] = true;
-	return dp[pos][mask];
+void Solve(void) {
+  cin >> n;
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++)
+      cin >> likes[i][j];
+  int mx = 1 << n;
+  for (int i = 0; i < mx; i++)
+    for (int j = 0; j < n; j++)
+      is_done[i][j] = false;
+  cout << DP(0, 0) << '\n';
 }
 
-void solve() {
-	cin >> n;
-	clear(n);
-	For(i, 0, n) For(j, 0, n) cin >> likes[i][j];
-	cout << DP(0, 0) << endl;
-}
-
-int main() {
-	fastio;
-	int t = 1;
-	cin >> t;
-	while (t--) solve();
-	return 0;
+int main(void) {
+  int t;
+  cin >> t;
+  while (t--) Solve();
+  return 0;
 }
