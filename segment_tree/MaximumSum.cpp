32594891c41da7c5dba6da0dced168361b8d5110
ManuelLoaizaVasquez <manulova999@gmail.com>
2021-02-09 21:08:34 -0500

    Dos mayores en un rango

--- segment_tree/MaximumSum.cpp
+++ segment_tree/MaximumSum.cpp
@@ -1,88 +1,90 @@
+//https://www.spoj.com/problems/KGSS/
 #include <bits/stdc++.h>
 using namespace std;
 
 typedef long long Long;
 typedef pair<Long, Long> Pair;
-const Long N = 1e5;
 
-struct Data {
-    Pair max1, max2;
-    Data(){}
-    Data(Long val, Long pos) {
-        max1.first = val;
-        max1.second = pos;
-        max2.first = 0;
-        max2.second = -1;
-    }
-} t[4 * N];
+struct Node {
+  Pair max1, max2;
+  Node(void) {}
+  Node(Long val, Long pos) {
+    max1.first = val;
+    max1.second = pos;
+    max2.first = 0;
+    max2.second = -1;
+  }
+};
 
-Data combine(Data x, Data y) {
+const int N = 1e5;
+
+struct SegmentTree {
+  Node tree[4 * N];
+  Node Merge(const Node& x, const Node& y) {
     vector<Pair> aux;
-    aux.push_back(x.max1);
-    aux.push_back(x.max2);
-    aux.push_back(y.max1);
-    aux.push_back(y.max2);
+    aux.push_back({x.max1});
+    aux.push_back({x.max2});
+    aux.push_back({y.max1});
+    aux.push_back({y.max2});
     sort(aux.begin(), aux.end());
-    Data ans;
+    Node ans;
     ans.max1 = aux[3];
     ans.max2 = aux[2];
     return ans;
-}
-
-void build(vector<Long> &a, Long id, Long tl, Long tr) {
+  }
+  void Build(const vector<Long>& a, int id, int tl, int tr) {
     if (tl == tr) {
-        t[id] = Data(a[tl], tl);
+      tree[id] = Node(a[tl], tl);
     } else {
-        Long tm = (tl + tr) / 2;
-        build(a, 2 * id, tl, tm);
-        build(a, 2 * id + 1, tm + 1, tr);
-        t[id] = combine(t[2 * id], t[2 * id + 1]);
+      int tm = (tl + tr) / 2;
+      Build(a, 2 * id, tl, tm);
+      Build(a, 2 * id + 1, tm + 1, tr);
+      tree[id] = Merge(tree[2 * id], tree[2 * id + 1]);
     }
-}
-
-Data query(Long l, Long r, Long id, Long tl, Long tr) {
-    if (l <= tl and tr <= r) return t[id];
-    Long tm = (tl + tr) / 2;
-    if (tm < l) return query(l, r, 2 * id + 1, tm + 1, tr);
-    if (r < tm + 1) return query(l, r, 2 * id, tl, tm);
-    return combine(query(l, r, 2 * id, tl, tm), query(l, r, 2 * id + 1, tm + 1, tr));
-}
-
-void update(Long pos, Long val, Long id, Long tl, Long tr) {
+  }
+  void Update(int pos, Long val, int id, int tl, int tr) {
     if (tl == tr) {
-        t[id] = Data(val, pos);
+      tree[id] = Node(val, tl);
     } else {
-        Long tm = (tl + tr) / 2;
-        if (pos <= tm) update(pos, val, 2 * id, tl, tm);
-        else update(pos, val, 2 * id + 1, tm + 1, tr);
-        t[id] = combine(t[2 * id], t[2 * id + 1]);
+      int tm = (tl + tr) / 2;
+     if (pos <= tm) {
+       Update(pos, val, 2 * id, tl, tm);
+     } else {
+       Update(pos, val, 2 * id + 1, tm + 1, tr);
+     }
+     tree[id] = Merge(tree[2 * id], tree[2 * id + 1]);
     }
-}
+  }
+  Node Query(int l, int r, int id, int tl, int tr) {
+    if (l <= tl && tr <= r) return tree[id];
+    int tm = (tl + tr) / 2;
+    if (r <= tm) return Query(l, r, 2 * id, tl, tm);
+    if (tm < l) return Query(l, r, 2 * id + 1, tm + 1, tr);
+    return Merge(Query(l, r, 2 * id, tl, tm), Query(l, r, 2 * id + 1, tm + 1, tr));
+  }
+} st;
 
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    Long n;
-    cin >> n;
-    vector<Long> a;
-    for (int i = 0; i < n; i++) {
-        Long x;
-        cin >> x;
-        a.push_back(x);
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(nullptr);
+  int n;
+  cin >> n;
+  vector<Long> a(n);
+  for (int i = 0; i < n; i++) cin >> a[i];
+  st.Build(a, 1, 0, n - 1);
+  int q;
+  cin >> q;
+  while (q--) {
+    char t;
+    int l, r;
+    cin >> t >> l >> r;
+    if (t == 'U') {
+      st.Update(l - 1, r, 1, 0, n - 1);
+    } else {
+      Node query = st.Query(l - 1, r - 1, 1, 0, n - 1);
+      Long sum = query.max1.first + query.max2.first;
+      cout << sum << '\n';
     }
-    build(a, 1, 0, n - 1);
-    Long q;
-    cin >> q;
-    while (q--) {
-        char t;
-        Long l, r;
-        cin >> t >> l >> r;
-        if (t == 'U') update(l - 1, r, 1, 0, n - 1);
-        else {
-            Data ans = query(l - 1, r - 1, 1, 0, n - 1);
-            Long res = ans.max1.first + ans.max2.first;
-            cout << res << endl;
-        }
-    }
-    return 0;
+  }
+  return 0;
 }
