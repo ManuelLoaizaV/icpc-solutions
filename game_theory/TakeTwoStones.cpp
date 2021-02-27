1114f42c0023f21f2788807d3a0c57331ccffb9e
ManuelLoaizaVasquez <manulova999@gmail.com>
2021-02-27 13:27:34 -0500

    Juego trivial

---
+++ game_theory/TakeTwoStones.cpp
@@ -0,0 +1,37 @@
+//https://open.kattis.com/problems/twostones
+#include <bits/stdc++.h>
+#include <unistd.h>
+#define FAST_IO ios::sync_with_stdio(0);cin.tie(NULL)
+#define all(v) (v).begin(),(v).end()
+using namespace std;
+
+typedef long long Long;
+typedef long double Double;
+typedef unsigned long long ULong;
+typedef pair<Long, Long> Pair;
+
+const int N = 1e6;
+const Long INF = 1e18;
+const Double EPS = 1e-9;
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+Long random(Long a, Long b) { return uniform_int_distribution<Long> (a, b) (rng); }
+
+void Solve(void) {
+  Long n;
+  cin >> n;
+  if (n & 1) {
+    cout << "Alice" << '\n';
+  } else {
+    cout << "Bob" << '\n';
+  }
+}
+
+int main(void) {
+  FAST_IO;
+  int t = 1;
+  //cin >> t;
+  while (t--) Solve();
+  return 0;
+}
+
