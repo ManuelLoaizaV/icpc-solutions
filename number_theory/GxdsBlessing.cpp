f6823ff77255c03e02ecc922e57c3b614b9296d4
ManuelLoaizaVasquez <manulova999@gmail.com>
2021-10-03 23:19:30 -0500

    phi, dirichlet

---
+++ number_theory/GxdsBlessing.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long Long;
+
+const int N = 1e7;
+
+void Solve(void) {
+  int n;
+  cin >> n;
+
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int tt;
+  cin >> tt;
+  while (tt--) Solve();
+  return 0;
+}
