e16dec5871277b5c842ed203ff6346b0ade4bd0e
Manuel Loaiza <manuel.loaiza@pucp.edu.pe>
2022-04-17 08:13:01 -0500

    greedy

---
+++ number_theory/GcdVsLcm.cpp
@@ -0,0 +1,36 @@
+// https://codeforces.com/contest/1665/problem/A
+#include <bits/stdc++.h>
+#include <unistd.h>
+#define debug(x) cout << #x << " = " << x << endl
+using namespace std;
+
+typedef long long Long;
+typedef long double Double;
+typedef unsigned long long ULong;
+typedef pair<Long, Long> Pair;
+
+const int N = 1e6;
+const Long INF = 1e18;
+const Double EPS = 1e-9;
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+Long random(Long a, Long b) { return uniform_int_distribution<Long>(a, b)(rng); }
+
+void Solve(void) {
+  Long n;
+  cin >> n;
+  Long a = 1;
+  Long b = n - 3;
+  Long c = 1;
+  Long d = 1;
+  cout << a << " " << b << " " << c << " " << d << '\n';
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int t = 1;
+  cin >> t;
+  while (t--) Solve();
+  return 0;
+}
