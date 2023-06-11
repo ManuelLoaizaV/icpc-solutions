832a119645e1323162bb570c0803722dd8f1f537
Manuel Loaiza <manuel.loaiza@pucp.edu.pe>
2023-06-11 18:26:21 -0500

    binary search

--- binary-search/Hamburgers.cpp
+++ binary-search/Hamburgers.cpp
@@ -1,68 +1,54 @@
-//https://codeforces.com/problemset/problem/371/C
-#include <bits/stdc++.h>
-#include <unistd.h>
-#define FAST_IO ios::sync_with_stdio(0);cin.tie(NULL)
-#define all(v) (v).begin(),(v).end()
+// https://codeforces.com/problemset/problem/371/C
+#include <iostream>
+#include <map>
+#include <vector>
 using namespace std;
 
 typedef long long Long;
-typedef long double Double;
-typedef unsigned long long ULong;
-typedef pair<Long, Long> Pair;
 
-const int N = 1e6;
-const Long INF = 1e12 + 105;
-const Double EPS = 1e-9;
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  string recipe;
+  cin >> recipe;
 
-mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
-Long random(Long a, Long b) { return uniform_int_distribution<Long> (a, b) (rng); }
+  map<char, int> indexes = {
+    {'B', 0},
+    {'S', 1},
+    {'C', 2}
+  };
 
-Long n[3];
-Long cur[3];
-Long price[3];
-Long money;
+  vector<Long> quantities(3, 0LL);
+  for (char ingredient: recipe) {
+    quantities[indexes[ingredient]]++;
+  }
 
-bool Check(Long target) {
-  Long sum = 0;
+  vector<Long> kitchen(3);
   for (int i = 0; i < 3; i++) {
-    Long needed = n[i] * target;
-    if (cur[i] < needed) sum += price[i] * (needed - cur[i]);
+    cin >> kitchen[i];
   }
-  return sum <= money;
-}
 
-void Solve(void) {
-  string s;
-  cin >> s;
-  for (char c : s) {
-    if (c == 'B') {
-      n[0]++;
-    } else if (c == 'S') {
-      n[1]++;
-    } else {
-      n[2]++;
-    }
+  vector<Long> prices(3);
+  for (int i = 0; i < 3; i++) {
+    cin >> prices[i];
   }
-  for (int i = 0; i < 3; i++) cin >> cur[i];
-  for (int i = 0; i < 3; i++) cin >> price[i];
+
+  Long money;
   cin >> money;
   Long l = 0;
-  Long r = INF;
+  Long r = 10000000000000LL;
   while (r - l > 1) {
     Long m = (l + r) / 2;
-    if (Check(m)) {
+    Long cost = 0;
+    for (int i = 0; i < 3; i++) {
+      cost += max(0LL, m * quantities[i] - kitchen[i]) * prices[i];
+    }
+    if (cost <= money) {
       l = m;
     } else {
       r = m;
     }
   }
   cout << l << '\n';
-}
-
-int main(void) {
-  FAST_IO;
-  int t = 1;
-  //cin >> t;
-  while (t--) Solve();
   return 0;
 }
