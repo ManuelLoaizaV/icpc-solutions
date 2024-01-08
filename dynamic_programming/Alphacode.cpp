3ed63a2289140a65e1be6d94df87dd3864135cc2
Manuel Loaiza <manuel.loaiza.vasquez@gmail.com>
2024-01-08 10:59:41 -0500

    trivial

--- dynamic_programming/Alphacode.cpp
+++ dynamic_programming/Alphacode.cpp
@@ -1,48 +1,59 @@
-//https://www.spoj.com/problems/ACODE/
-#include <iostream>
-#include <string>
+// https://www.spoj.com/problems/ACODE/en/
+#include <bits/stdc++.h>
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << "[" << #x << "]: " << x << endl
+#else
+#define debug(x) 69
+#endif
+
 using namespace std;
 
 typedef long long Long;
+typedef long double Double;
+typedef pair<Long, Long> Pair;
 
-const int N = 5e3;
-const int ALPHABET = 26;
+const int N = 1e6;
+const Long MOD = 998244353LL;
+const Long INF = 1e18;
+const Double PI = acos(-1);
+const Double EPS = 1e-9;
 
-string s;
-int a[N];
-int n;
-bool is_done[N];
-Long dp[N];
+const char A = 'a';
+const char ZERO = '0';
+const char ONE = '1';
+const char TWO = '2';
+const char SIX = '6';
+const char ENDL = '\n';
 
-Long DP(int pos) {
-  if (pos == n) return 1;
-  if (is_done[pos]) return dp[pos];
-  dp[pos] = 0;
-  if (a[pos] > 0) {
-    dp[pos] = DP(pos + 1);
-    if (pos < n - 1) {
-      int num = 10 * a[pos] + a[pos + 1];
-      if (num <= ALPHABET) dp[pos] += DP(pos + 2);
+const string YES = "YES";
+const string NO = "NO";
+
+void Solve(const string& s) {
+  int n = (int)s.size();
+  // dp(i) = number of encodings of s[i..n)
+  vector<Long> dp(n + 1, 0LL);
+  // Base case: Empty string has 1 encoding
+  dp[n] = 1LL;
+  for (int i = n - 1; i >= 0; i--) {
+    if (s[i] != ZERO) {
+      dp[i] += dp[i + 1];
+    }
+    if (i < n - 1 && (s[i] == ONE || s[i] == TWO && s[i + 1] <= SIX)) {
+      dp[i] += dp[i + 2];
     }
   }
-  is_done[pos] = true;
-  return dp[pos];
-}
-
-void Solve(void) {
-  n = s.size();
-  for (int i = 0; i < n; i++) a[i] = s[i] - '0';
-  for (int i = 0; i < n; i++) is_done[i] = false;
-  cout << DP(0) << '\n';
+  cout << dp[0] << ENDL;
 }
 
 int main(void) {
-  ios::sync_with_stdio(0);
-  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cin.tie(0);
   while (true) {
+    string s;
     cin >> s;
-    if (s.size() == 1 && s[0] == '0') break;
-    Solve();
+    if (s == "0") break;
+    Solve(s);
   }
   return 0;
 }
