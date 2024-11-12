ffa89dc4fd69837a1fcdd297238802aad01f1e4f
Manuel Loaiza <manuel.loaiza.vasquez@gmail.com>
2024-11-12 18:16:54 -0500

    math

---
+++ number_theory/MaximumGCD.cpp
@@ -0,0 +1,14 @@
+// https://codeforces.com/problemset/problem/1370/A
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        cout << n / 2 << endl;
+    }
+    return 0;
+}
